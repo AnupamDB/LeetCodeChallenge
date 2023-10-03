8076a4f4da06627a563f2f7f7bace4c3bfcac268
Anupam Dev Burman <107240384+AnupamDB@users.noreply.github.com>
2023-10-03 11:00:46 +0530

    Create goodpairs.cpp

---
+++ goodpairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int c=0;
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j]) c+=1;
+            }
+        }
+        return c;
+    }
+};
